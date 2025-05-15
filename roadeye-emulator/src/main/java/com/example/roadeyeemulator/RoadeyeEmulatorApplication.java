2064802ac81c0a16df513340f8ff945d56e2ce8d
1eehaewon <lhw2112@nate.com>
2025-05-15 17:26:29 +0900

    feat : 에뮬레이터 테스트 뷰 생성

---
+++ roadeye-emulator/src/main/java/com/example/roadeyeemulator/RoadeyeEmulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.roadeyeemulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadeyeEmulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadeyeEmulatorApplication.class, args);
+    }
+
+}
---
+++ roadeye-emulator/src/test/java/com/example/roadeyeemulator/RoadeyeEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadeyeemulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadeyeEmulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
