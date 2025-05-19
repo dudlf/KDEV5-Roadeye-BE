2da9f71a26ddd0c671820612826ec0fd1f8a4c52
1eehaewon <lhw2112@nate.com>
2025-05-19 11:47:01 +0900

    refactor : 초기 설정 수정

--- roadeye-emulator/src/main/java/org/re/emulator/controller/EmulatorController.java
+++ roadeye-emulator/src/main/java/org/re/emulator/controller/EmulatorController.java
@@ -1,4 +1,4 @@
-package com.example.roadeyeemulator.controller;
+package org.re.emulator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
