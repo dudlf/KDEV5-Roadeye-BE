9f505a502965dfdbb2fdd766aa0dd9797d75c6ff
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-05-19 16:14:12 +0900

    build: jib plugin 모듈 설정 추가

---
+++ roadeye-plugin/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.4.4")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:2.1.20")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:2.1.20")
+    implementation("com.google.cloud.tools:jib-gradle-plugin:3.4.5")
+}
