749483c5af0b2e24484c3d586414c19f593dd0aa
1eehaewon <lhw2112@nate.com>
2025-05-22 14:56:22 +0900

    feat : Car Entity 추가

---
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/domain/CarStatus.java
@@ -0,0 +1,28 @@
+package org.re.hq.car.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class CarStatus {
+
+    private Integer batteryVoltage;
+
+    private Integer ignitionStatus;
+
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID activeTuid;
+
+    public CarStatus(Integer batteryVoltage, Integer ignitionStatus, UUID activeTuid) {
+        this.batteryVoltage = batteryVoltage;
+        this.ignitionStatus = ignitionStatus;
+        this.activeTuid = activeTuid;
+    }
+}
+
