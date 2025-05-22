90ff4063002b1b3cb19eb1a9cff5910ef5767a75
1eehaewon <lhw2112@nate.com>
2025-05-22 18:01:15 +0900

    feat : 최사별 차량 목록 조회

--- roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/repository/CarRepository.java
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/repository/CarRepository.java
@@ -1,7 +1,10 @@
 package org.re.hq.car.repository;
 
 import org.re.hq.car.domain.Car;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
+    Page<Car> findByCompanyId(Integer companyId, Pageable pageable);
 }
--- roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/service/CarService.java
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/service/CarService.java
@@ -4,6 +4,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.re.hq.car.domain.Car;
 import org.re.hq.car.repository.CarRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,4 +24,11 @@ public class CarService {
         return carRepository.save(car);
     }
 
+    /**
+     * 회사별 차량 목록 조회 (10개씩 페이징)
+     **/
+    public Page<Car> getCarsByCompany(Integer companyId, int page) {
+        Pageable pageable = PageRequest.of(page, 10); // 페이지당 10개
+        return carRepository.findByCompanyId(companyId, pageable);
+    }
 }
