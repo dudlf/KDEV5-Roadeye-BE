ab6fc6aa0ffee444cba32224058c8f16adc020dd
dudlf <dudlf016@gmail.com>
2025-05-23 11:05:17 +0900

    refactor: boolean 이름에서 'is' 제거

--- roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/security/userdetails/CompanyUserDetails.java
+++ roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/security/userdetails/CompanyUserDetails.java
@@ -15,53 +15,53 @@ import java.util.stream.Stream;
 @Getter
 public class CompanyUserDetails implements UserDetails {
     private static final Collection<? extends GrantedAuthority> DEFAULT_AUTHORITIES
-        = Collections.unmodifiableCollection(AuthorityUtils.createAuthorityList(AuthMemberType.USER.getValue()));
+            = Collections.unmodifiableCollection(AuthorityUtils.createAuthorityList(AuthMemberType.USER.getValue()));
 
     private final String username;
     private final String password;
 
     private final Collection<? extends GrantedAuthority> authorities;
 
-    private final boolean isAccountNonExpired;
-    private final boolean isAccountNonLocked;
-    private final boolean isCredentialsNonExpired;
-    private final boolean isEnabled;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
 
     private CompanyUserDetails(
-        String username,
-        String password,
-        Collection<? extends GrantedAuthority> authorities,
-        boolean isAccountNonExpired,
-        boolean isAccountNonLocked,
-        boolean isCredentialsNonExpired,
-        boolean isEnabled
+            String username,
+            String password,
+            Collection<? extends GrantedAuthority> authorities,
+            boolean accountNonExpired,
+            boolean accountNonLocked,
+            boolean credentialsNonExpired,
+            boolean enabled
     ) {
         this.username = username;
         this.password = password;
         this.authorities = Collections.unmodifiableCollection(authorities);
-        this.isAccountNonExpired = isAccountNonExpired;
-        this.isAccountNonLocked = isAccountNonLocked;
-        this.isCredentialsNonExpired = isCredentialsNonExpired;
-        this.isEnabled = isEnabled;
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
     }
 
     public static UserDetails from(Employee user) {
         return new CompanyUserDetails(
-            user.getCredentials().loginId(),
-            user.getCredentials().password(),
-            createAuthorities(user),
-            true,
-            true,
-            true,
-            true
+                user.getCredentials().loginId(),
+                user.getCredentials().password(),
+                createAuthorities(user),
+                true,
+                true,
+                true,
+                true
         );
     }
 
     private static Collection<? extends GrantedAuthority> createAuthorities(Employee user) {
         return Stream.concat(
-            DEFAULT_AUTHORITIES.stream(),
-            Stream.of(user.getMetadata().getRole())
-                .map((r) -> new SimpleGrantedAuthority(r.name()))
+                DEFAULT_AUTHORITIES.stream(),
+                Stream.of(user.getMetadata().getRole())
+                        .map((r) -> new SimpleGrantedAuthority(r.name()))
         ).toList();
     }
 }
