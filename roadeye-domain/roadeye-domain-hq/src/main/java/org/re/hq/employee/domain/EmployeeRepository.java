eb64680d7e0deac1ac85328323d1f2b2eb821dc9
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-05-23 10:35:45 +0900

    feat: 직원 정보 조회 기능 구현

--- roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/employee/domain/EmployeeRepository.java
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/employee/domain/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package org.re.hq.employee.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByTenantId(Long tenantId);
 
     Optional<Employee> findByIdAndTenantId(Long id, Long tenantId);
+
+    Page<Employee> findByTenantId(Long tenantId, Pageable pageable);
 }
