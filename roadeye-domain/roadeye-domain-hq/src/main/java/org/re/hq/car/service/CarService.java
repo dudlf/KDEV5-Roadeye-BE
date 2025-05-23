2e0fd1422058331686270b52a7873e122cffc5c7
1eehaewon <lhw2112@nate.com>
2025-05-23 10:33:07 +0900

    refactor : 타입 수정 & 필드명 수정

--- roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/service/CarService.java
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/service/CarService.java
@@ -3,10 +3,8 @@ package org.re.hq.car.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.re.hq.car.domain.Car;
+import org.re.hq.car.domain.CarProfile;
 import org.re.hq.car.repository.CarRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,16 +17,9 @@ public class CarService {
     /**
      * 차량 등록
      **/
-    public Car createCar(Integer companyId, String carName, String carImgUrl, String carNumber, Integer carMileageInit) {
-        Car car = Car.of(companyId, carName, carImgUrl, carNumber, carMileageInit);
+    public Car createCar(Long companyId, CarProfile profile, int initial) {
+        Car car = Car.of(companyId, profile, initial);
         return carRepository.save(car);
     }
 
-    /**
-     * 회사별 차량 목록 조회 (10개씩 페이징)
-     **/
-    public Page<Car> getCarsByCompany(Integer companyId, int page) {
-        Pageable pageable = PageRequest.of(page, 10); // 페이지당 10개
-        return carRepository.findByCompanyId(companyId, pageable);
-    }
 }
