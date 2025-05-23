da9213a38eac1b6444e5e35602ee9265135b1c6f
1eehaewon <lhw2112@nate.com>
2025-05-23 10:30:59 +0900

    refactor : 필드명 수정 및 시동상태 ENUM 분리

--- roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/domain/CarStatus.java
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/domain/CarStatus.java
@@ -2,6 +2,8 @@ package org.re.hq.car.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +17,15 @@ public class CarStatus {
 
     private Integer batteryVoltage;
 
-    private Integer ignitionStatus;
+    @Enumerated(EnumType.STRING)
+    private CarIgnitionStatus ignition;
 
     @Column(columnDefinition = "BINARY(16)")
     private UUID activeTuid;
 
-    public CarStatus(Integer batteryVoltage, Integer ignitionStatus, UUID activeTuid) {
+    public CarStatus(Integer batteryVoltage, CarIgnitionStatus ignition, UUID activeTuid) {
         this.batteryVoltage = batteryVoltage;
-        this.ignitionStatus = ignitionStatus;
+        this.ignition = ignition;
         this.activeTuid = activeTuid;
     }
 }
