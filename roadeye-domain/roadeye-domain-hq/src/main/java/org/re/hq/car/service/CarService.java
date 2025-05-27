6629d659d25db4a453d34f76a38c9bb438418bb4
dudlf <dudlf016@gmail.com>
2025-05-27 14:02:07 +0900

    refactor: CarService @Transactional jakarta에서 spring으로 변경

--- roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/service/CarService.java
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/car/service/CarService.java
@@ -1,6 +1,5 @@
 package org.re.hq.car.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.re.hq.car.domain.Car;
 import org.re.hq.car.dto.CarCreationCommand;
@@ -11,6 +10,7 @@ import org.re.hq.domain.common.EntityLifecycleStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
