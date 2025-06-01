65d3fa1605bf34dd457aa8b28df109633ad38bd7
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-06-01 13:52:36 +0900

    refactor: employeeservice 패키지 위치 변경

--- roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/employee/service/EmployeeService.java
+++ roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/employee/service/EmployeeService.java
@@ -1,11 +1,10 @@
-package org.re.hq.employee;
+package org.re.hq.employee.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.re.hq.employee.domain.Employee;
 import org.re.hq.employee.domain.EmployeeCredentials;
 import org.re.hq.employee.domain.EmployeeMetadata;
-import org.re.hq.employee.service.EmployeeDomainService;
 import org.re.hq.tenant.TenantId;
 import org.springframework.stereotype.Service;
 
@@ -20,18 +19,18 @@ public class EmployeeService {
 
         employeeDomainService.validateExistsRootAccount(tenantId.value());
         var employee = Employee.createRoot(
-                tenantId.value(),
-                credentials,
-                metadata
+            tenantId.value(),
+            credentials,
+            metadata
         );
         return employeeDomainService.create(employee);
     }
 
     public Long createNormal(TenantId tenantId, EmployeeCredentials credentials, EmployeeMetadata metadata) {
         var employee = Employee.createNormal(
-                tenantId.value(),
-                credentials,
-                metadata
+            tenantId.value(),
+            credentials,
+            metadata
         );
         return employeeDomainService.create(employee);
     }
