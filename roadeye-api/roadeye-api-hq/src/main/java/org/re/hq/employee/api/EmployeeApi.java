815fa254f599947d82e76a64cc78dd3ed703989f
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-06-01 15:07:07 +0900

    feat: 직원 관리 api 추가

---
+++ roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/employee/api/EmployeeApi.java
@@ -0,0 +1,52 @@
+package org.re.hq.employee.api;
+
+import lombok.RequiredArgsConstructor;
+import org.re.hq.employee.api.payload.AccountStatus;
+import org.re.hq.employee.api.payload.EmployeeCreateRequest;
+import org.re.hq.employee.api.payload.EmployeeSearchResponse;
+import org.re.hq.employee.api.payload.EmployeeUpdateRequest;
+import org.re.hq.employee.service.EmployeeService;
+import org.re.hq.tenant.TenantId;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeApi {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    public void create(TenantId tenantId, @RequestBody EmployeeCreateRequest employeeCreateRequest) {
+        employeeService.createNormal(
+            tenantId,
+            employeeCreateRequest.toCredentials(),
+            employeeCreateRequest.toMetadata()
+        );
+    }
+
+    @PutMapping("/{employeeId}")
+    public void update(TenantId tenantId, @PathVariable Long employeeId, @RequestBody EmployeeUpdateRequest employeeUpdateRequest) {
+        employeeService.update(tenantId, employeeId, employeeUpdateRequest.toCommand());
+    }
+
+    @PatchMapping("/{employeeId}/{status}")
+    public void patch(TenantId tenantId, @PathVariable Long employeeId, @PathVariable AccountStatus status) {
+        employeeService.changeStatus(tenantId, employeeId, status);
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public void delete(TenantId tenantId, @PathVariable Long employeeId) {
+        employeeService.delete(tenantId, employeeId);
+    }
+
+    @GetMapping
+    public PagedModel<EmployeeSearchResponse> getAll(TenantId tenantId, Pageable pageable) {
+        return new PagedModel<>(employeeService.readAll(tenantId, pageable)
+            .map(EmployeeSearchResponse::from)
+        );
+    }
+
+}
--- roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/employee/service/EmployeeService.java
+++ roadeye-api/roadeye-api-hq/src/main/java/org/re/hq/employee/service/EmployeeService.java
@@ -2,10 +2,15 @@ package org.re.hq.employee.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.re.hq.employee.api.payload.AccountStatus;
 import org.re.hq.employee.domain.Employee;
 import org.re.hq.employee.domain.EmployeeCredentials;
 import org.re.hq.employee.domain.EmployeeMetadata;
+import org.re.hq.employee.dto.UpdateEmployeeCommand;
 import org.re.hq.tenant.TenantId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,5 +42,26 @@ public class EmployeeService {
 
     public Employee read(TenantId tenantId, Long employeeId) {
         return employeeDomainService.read(tenantId.value(), employeeId);
+    }
+
+    public void changeStatus(TenantId tenantId, Long employeeId, @NonNull AccountStatus status) {
+        var employee = employeeDomainService.read(tenantId.value(), employeeId);
+
+        switch (status) {
+            case ENABLE -> employee.enable();
+            case DISABLE -> employee.disable();
+        }
+    }
+
+    public void delete(TenantId tenantId, Long employeeId) {
+        employeeDomainService.delete(tenantId.value(), employeeId);
+    }
+
+    public Page<Employee> readAll(TenantId tenantId, Pageable pageable) {
+        return employeeDomainService.readAll(tenantId.value(), pageable);
+    }
+
+    public void update(TenantId tenantId, Long employeeId, UpdateEmployeeCommand command) {
+        employeeDomainService.updateMetadata(tenantId.value(), employeeId, command);
     }
 }
