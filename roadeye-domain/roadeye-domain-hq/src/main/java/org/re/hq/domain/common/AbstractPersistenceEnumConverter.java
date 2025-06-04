59ec1b25ea258aa9912232b31052c8077316c1cc
dudlf <dudlf016@gmail.com>
2025-06-04 16:32:43 +0900

    refactor: JPA 공통 enum converter

---
+++ roadeye-domain/roadeye-domain-hq/src/main/java/org/re/hq/domain/common/AbstractPersistenceEnumConverter.java
@@ -0,0 +1,53 @@
+package org.re.hq.domain.common;
+
+import jakarta.persistence.AttributeConverter;
+
+import java.util.Objects;
+
+@SuppressWarnings("ConverterNotAnnotatedInspection")
+public abstract class AbstractPersistenceEnumConverter<T extends Enum<T> & PersistenceEnum<E>, E> implements AttributeConverter<T, E> {
+    private final Class<T> enumClass;
+    private final boolean allowsNull;
+
+    public AbstractPersistenceEnumConverter(Class<T> enumClass) {
+        this(enumClass, false);
+    }
+
+    public AbstractPersistenceEnumConverter(Class<T> enumClass, boolean allowsNull) {
+        this.enumClass = enumClass;
+        this.allowsNull = allowsNull;
+    }
+
+    @Override
+    public E convertToDatabaseColumn(T attribute) {
+        if (Objects.isNull(attribute)) {
+            if (allowsNull) {
+                return null;
+            } else {
+                throw new IllegalArgumentException("Null value not allowed for " + attribute.getClass().getSimpleName());
+            }
+        }
+        return attribute.getCode();
+    }
+
+    @Override
+    public T convertToEntityAttribute(E dbData) {
+        if (Objects.isNull(dbData)) {
+            if (allowsNull) {
+                return null;
+            } else {
+                throw new IllegalArgumentException("Null value not allowed for " + dbData.getClass().getSimpleName());
+            }
+        }
+        return getEnumInstance(dbData);
+    }
+
+    private T getEnumInstance(E dbData) {
+        for (T enumConstant : enumClass.getEnumConstants()) {
+            if (Objects.equals(enumConstant.getCode(), dbData)) {
+                return enumConstant;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant found for value: " + dbData);
+    }
+}
