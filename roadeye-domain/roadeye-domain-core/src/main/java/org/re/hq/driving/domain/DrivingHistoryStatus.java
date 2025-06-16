fe93f973d92535ecc41c82b5a10d244bd7be625b
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-06-16 17:11:21 +0900

    refactor: enum code from 부분 제거

--- roadeye-domain/roadeye-domain-core/src/main/java/org/re/hq/driving/domain/DrivingHistoryStatus.java
+++ roadeye-domain/roadeye-domain-core/src/main/java/org/re/hq/driving/domain/DrivingHistoryStatus.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.re.hq.domain.common.PersistenceEnum;
 
-import java.util.Arrays;
-
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum DrivingHistoryStatus implements PersistenceEnum<String> {
@@ -16,12 +14,5 @@ public enum DrivingHistoryStatus implements PersistenceEnum<String> {
     ;
     // @formatter:on
     private final String code;
-
-    public static DrivingHistoryStatus from(String value) {
-        return Arrays.stream(values())
-            .filter(it -> it.code.equals(value))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("Unknown DrivingHistoryStatus: "));
-    }
 
 }
