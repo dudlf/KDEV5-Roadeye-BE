4cffca65d8f2e8a2a01b2e05ec912ab38eea010c
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-06-16 14:05:34 +0900

    chore: 모듈명 변경 및 변경된 모듈 이름 적용

--- roadeye-domain/roadeye-domain-mdt-log/src/main/java/org/re/mdtlog/domain/MdtLogRepository.java
+++
@@ -1,9 +0,0 @@
-package org.re.mdtlog.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface MdtLogRepository extends JpaRepository<MdtLog, UUID> {
-
-}
