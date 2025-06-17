39098d47f42ec03fa2ccc3ddee9a359ee4bb212d
dudlf <dudlf016@gmail.com>
2025-06-17 20:12:45 +0900

    refactor: rename WebConfig

--- roadeye-api/roadeye-api-mdt-log-collector/src/main/java/org/re/config/WebMvcConfig.java
+++ roadeye-api/roadeye-api-mdt-log-collector/src/main/java/org/re/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MdtLogRequestTimeInfoResolver());
